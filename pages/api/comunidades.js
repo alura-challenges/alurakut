5d0258cd7735a97b1a8894435b408635e641a339
Mario Souto <soutomarios@gmail.com>
2021-07-15 09:05:47 -0300

    final aula 04

---
+++ pages/api/comunidades.js
@@ -0,0 +1,29 @@
+import { SiteClient } from 'datocms-client';
+
+export default async function recebedorDeRequests(request, response) {
+    if(request.method === 'POST') {
+        const TOKEN = '3a9782340b8b4df0d359bb792b880b';
+        const client = new SiteClient(TOKEN);
+        
+        // Validar os dados, antes de sair cadastrando
+        const registroCriado = await client.items.create({
+            itemType: "836335", // ID do Model de "Communities" criado pelo Dato
+            ...request.body,
+            // title: "Comunidade de Teste",
+            // imageUrl: "https://github.com/omariosouto.png",
+            // creatorSlug: "omariosouto"
+        })
+    
+        console.log(registroCriado);
+    
+        response.json({
+            dados: 'Algum dado qualquer',
+            registroCriado: registroCriado,
+        })
+        return;
+    }
+
+    response.status(404).json({
+        message: 'Ainda não temos nada no GET, mas no POST tem!'
+    })
+}
